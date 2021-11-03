3cabcea9d0615cf9a6350f059ddd4633c012e683
nade-harlow <omonadefranklyn@gmail.com>
2021-11-04 00:35:39 +0100

    refactored

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+  render(){
+    const name= "Jon"
+    const loading= false;
+    const showName= false;
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {loading ? <h4>Loading...</h4> : showName ? <h1>Hello {name}</h1> : null}
     </div>
   );
+  }  
 }
 
 export default App;
