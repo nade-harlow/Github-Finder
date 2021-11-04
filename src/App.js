99c2722413993486231d8c8e3aeae4a14ecf10c1
nade-harlow <omonadefranklyn@gmail.com>
2021-11-04 01:15:34 +0100

    added nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component{
   render(){
-    const name= "Jon"
-    const loading= false;
-    const showName= false;
     return (
     <div className="App">
-     {loading ? <h4>Loading...</h4> : showName ? <h1>Hello {name}</h1> : null}
+     <Navbar / >
     </div>
   );
   }  
