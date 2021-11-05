d488ab02af6362dc01ab33cb51c854f8020131eb
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2021-11-05 23:35:19 +0100

    changed class components to function componenet

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,14 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class Useritem extends Component {
-   state={
-        id: 'id',
-        login: 'mojombo',
-        avatar_url:'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url:'https://github.com/mojombo'
-    }
-    render() {
-        const {login,avatar_url,html_url }= this.state;
+function Useritem ({user:{login,avatar_url,html_url }}) {
         return (
             <div className="card text-center">
                <img src={avatar_url} alt="" className="round-img" style={{width:"60px"}}
@@ -20,7 +13,9 @@ class Useritem extends Component {
                 </div>
             </div>
         )
-    }
-}
+};
 
+Useritem.propTypes ={
+    user: PropTypes.object.isRequired,
+};
 export default Useritem
