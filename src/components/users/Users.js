c2f76594480806443329c4f6045154d1afdb803d
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2021-11-06 00:46:30 +0100

    chaged class to function

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,26 @@
-import React, { Component } from 'react'
-import Useritem from './Useritem'
+import React from 'react';
+import Useritem from './Useritem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-state = {
-    users:[
-        {
-            id: '1',
-            login: 'mojombo',
-            avatar_url:'https://avatars.githubusercontent.com/u/1?v=4',
-            html_url:'https://github.com/mojombo'
-        },
-        {
-            id: '2',
-            login: 'defunkt',
-            avatar_url:'https://avatars.githubusercontent.com/u/2?v=4',
-            html_url:'https://github.com/defunkt'
-        },
-        {
-            id: '3',
-            login: 'pjhyett',
-            avatar_url:'https://avatars.githubusercontent.com/u/3?v=4',
-            html_url:'https://github.com/pjhyett'
+
+function Users ({users, loading}) {
+        if (loading){
+            return <Spinner/>
+        }else{
+            return (
+                <div style={userStyle}>
+                    {users.map(user => (
+                    <Useritem key={user.id} user={user}/>
+                    ))}
+                </div>
+            )
         }
-    ]
 }
 
-    render() {
-        return (
-            <div style={userStyle}>
-                {this.state.users.map(user => (
-                   <Useritem key={user.id} user={user}/>
-                ))}
-            </div>
-        )
-    }
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 const userStyle ={
