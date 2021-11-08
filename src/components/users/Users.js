fa249c8df3d62d1a9e1ab25d352b8e9822ae921d
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2021-11-08 23:56:40 +0100

    .

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 
 function Users ({users, loading}) {
         if (loading){
-            return <Spinner/>
+            return <Spinner/> 
         }else{
             return (
                 <div style={userStyle}>
